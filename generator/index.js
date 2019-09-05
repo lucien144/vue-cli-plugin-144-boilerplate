0de494f99a30e55c1be2d4d614e84b9eae79d65f
Jan Blasko <honza@lucien144.net>
2019-09-05 15:36:16 +0200

    💪 XO rules with Prettier implemented
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- generator/index.js
+++ generator/index.js
@@ -8,7 +8,100 @@ module.exports = (api, options = {}) => {
 			'vue-svg-loader': '^0.12.0'
 		},
 		devDependencies: {
-			xo: '^0.24.0'
+			xo: '^0.24.0',
+			'eslint-config-xo-vue': '^2.0.2',
+			'eslint-plugin-vue': '^5.2.3',
+		},
+		scripts: {
+			lint: 'xo --fix',
+		},
+		prettier: {
+			'useTabs': true,
+			'singleQuote': true,
+			'tabWidth': 4,
+			'bracketSpacing': true
+		},
+		xo: {
+			'extends': [
+				'xo-vue',
+				'plugin:vue/recommended',
+				'prettier/unicorn'
+			],
+			'prettier': true,
+			'extensions': [
+				'js',
+				'vue'
+			],
+			'settings': {
+				'import/core-modules': [
+					'vuex'
+				]
+			},
+			'rules': {
+				'vue/html-indent': [
+					'error',
+					'tab'
+				],
+				'vue/max-attributes-per-line': [
+					2,
+					{
+						'singleline': 4
+					}
+				],
+				'import/extensions': [
+					'error',
+					'never',
+					{
+						'vue': 'ignorePackages',
+						'svg': 'always'
+					}
+				],
+				'import/no-unresolved': [
+					2,
+					{
+						'ignore': [
+							'vue$',
+							'svg$',
+							'^@'
+						]
+					}
+				],
+				'unicorn/filename-case': 0,
+				'no-unused-vars': [
+					'error',
+					{
+						'argsIgnorePattern': '^_'
+					}
+				],
+				'vue/component-name-in-template-casing': [
+					'error',
+					'PascalCase',
+					{
+						'registeredComponentsOnly': false,
+						'ignores': [
+							'transition'
+						]
+					}
+				],
+				'//new-cap': 'This allows for Vuex mutations like `this.SET_VALUE`',
+				'new-cap': [
+					'error',
+					{
+						'properties': false
+					}
+				],
+				'//unicorn/catch-error-name': 'Pass _error in try catch',
+				'unicorn/catch-error-name': [
+					'error',
+					{
+						'caughtErrorsIgnorePattern': '^_'
+					}
+				],
+				'object-curly-spacing': [
+					'error',
+					'always'
+				]
+			}
 		}
 	});
 
