83b2e4a7f22ee3fc6cca65373594efc535c6333d
Jan Blasko <honza@lucien144.net>
2021-02-11 14:17:29 +0100

    feat: ITCSS fonts & small tweaks

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
         loaderOptions: {
             sass: {
                 prependData: `
-                    @import "@/assets/styles/settings/_settings.bootstrap.scss";
                     @import "@/assets/styles/settings/_settings.tools.scss";
+                    @import "@/assets/styles/settings/_settings.bootstrap.scss";
                     @import "@/assets/styles/settings/_settings.colors.scss";
                     @import "@/assets/styles/settings/_settings.typo.scss";
                `
