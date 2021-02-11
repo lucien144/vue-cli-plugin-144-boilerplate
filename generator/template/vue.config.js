d1a23a57a3b1ab21f98e55a971b13fd2fd6e4e41
Jan Blasko <honza@lucien144.net>
2021-02-11 15:25:28 +0100

    feat: removing generic from ITCSS and base font updates

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -3,10 +3,11 @@ module.exports = {
         loaderOptions: {
             sass: {
                 prependData: `
-                    @import "@/assets/styles/settings/_settings.tools.scss";
                     @import "@/assets/styles/settings/_settings.bootstrap.scss";
+                    @import "@/assets/styles/settings/_settings.tools.scss";
                     @import "@/assets/styles/settings/_settings.colors.scss";
                     @import "@/assets/styles/settings/_settings.typo.scss";
+                    @import "@/assets/styles/settings/_settings.spacing.scss";
                `
             }
         }
